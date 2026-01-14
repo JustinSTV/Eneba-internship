817283d9423e082d79f275200235bf1bdf9c7447
Justin S <steponaviciusjustas@gmail.com>
2026-01-14 19:00:58 +0200

    corrected grid template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,7 +19,7 @@ const Container = styled.main`
 
   div.card-grid {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
     gap: 16px;
   }
 `;
